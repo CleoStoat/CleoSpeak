3359f159feb37c9e28dc3b4868dfbcd37eb2dc77
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2025-03-31 11:31:35 -0300

    Added change tab function

--- main.gd
+++ main.gd
@@ -1,9 +1,18 @@
 extends Control
 
+@onready var tabs_dict = {
+	0: %TTSTab,
+	1: %PresetsTab,
+	2: %ServerTab,
+}
 
 func _ready() -> void:
 	pass
 
 
 func _on_tab_bar_tab_changed(tab: int) -> void:
-	pass # Replace with function body.
+	for i in tabs_dict:
+		if i == tab:
+			tabs_dict[i].show()
+		else:
+			tabs_dict[i].hide()
