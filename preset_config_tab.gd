a0361b75f257220d79dbcbd401cc89b9583dc6d5
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2025-04-04 11:32:33 -0300

    Search bar for voice language selection

--- preset_config_tab.gd
+++ preset_config_tab.gd
@@ -3,6 +3,13 @@ extends Control
 @onready var select_preset_option_button := %SelectPresetOptionButton
 @onready var delete_preset_button := %DeletePresetButton
 @onready var preset_name_line_edit := %PresetNameLineEdit
+@onready var voice_language_line_edit := %VoiceLanguageLineEdit
+@onready var voice_language_item_list := %VoiceLanguageItemList
+@onready var selected_voice_label := %SelectedVoiceLabel
+@onready var slow_check_button := %SlowCheckButton
+
+const GTTS_VOICE_LANGUAGES = ["one", "two", "three", "four"]
+const EDGE_TTS_VOICE_LANGUAGES = ["1", "2", "3", "4"]
 
 signal presets_changed(new_presets: Array[Preset])
 
@@ -14,6 +21,8 @@ var presets: Array[Preset]:
 	set(value):
 		_presets = value
 		emit_signal("presets_changed", _presets)
+
+var available_voice_languages: Array[String] = []
 
 func _ready() -> void:
 	repopulate_select_preset()
@@ -68,3 +77,46 @@ func _on_preset_name_line_edit_text_changed(new_text: String) -> void:
 	else:
 		presets[index].name = new_text
 		repopulate_select_preset()
+
+
+
+func _on_voice_language_line_edit_text_changed(new_text: String) -> void:
+	filter_voices()
+
+
+func _on_tts_provider_option_button_item_selected(index: int) -> void:
+	voice_language_line_edit.clear()
+	var voice_languages
+	if index == 0:
+		voice_languages = GTTS_VOICE_LANGUAGES.duplicate()
+	elif index == 1:
+		voice_languages = EDGE_TTS_VOICE_LANGUAGES.duplicate()
+	
+	available_voice_languages.assign(voice_languages)
+	filter_voices()
+
+
+func filter_voices():
+	voice_language_item_list.clear()
+	
+	var filter = voice_language_line_edit.text
+	
+	var filtered_voices = available_voice_languages.filter(
+		func(voice: String): 
+			return voice.to_lower().begins_with(filter.to_lower())
+	)
+	
+	for voice in filtered_voices:
+		voice_language_item_list.add_item(voice)
+
+
+
+func _on_voice_language_item_list_item_selected(index: int) -> void:
+	if index == -1:
+		selected_voice_label.text = "None"
+	else:
+		selected_voice_label.text = voice_language_item_list.get_item_text(index)
+
+
+func _on_slow_check_button_pressed() -> void:
+	pass # Replace with function body.
