28f13c552fe833e66ca164bce916753e65be4154
CleoStoat <89475523+CleoStoat@users.noreply.github.com>
2025-04-05 00:09:16 -0300

    Proper seting loading for voice configuration settings when switching

--- preset_config_tab.gd
+++ preset_config_tab.gd
@@ -7,12 +7,12 @@ extends Control
 @onready var voice_language_item_list := %VoiceLanguageItemList
 @onready var selected_voice_label := %SelectedVoiceLabel
 @onready var slow_check_button := %SlowCheckButton
+@onready var tts_provider_option_button := %TTSProviderOptionButton
 
 const GTTS_VOICE_LANGUAGES = ["one", "two", "three", "four"]
 const EDGE_TTS_VOICE_LANGUAGES = ["1", "2", "3", "4"]
 
 signal presets_changed(new_presets: Array[Preset])
-
 var _presets: Array[Preset] = []
 
 var presets: Array[Preset]:
@@ -22,7 +22,25 @@ var presets: Array[Preset]:
 		_presets = value
 		emit_signal("presets_changed", _presets)
 
+signal selected_preset_index_changed(new_index: int)
+var _selected_preset_index: int
+
+var selected_preset_index: int:
+	get:
+		return _selected_preset_index
+	set(value):
+		_selected_preset_index = value
+		emit_signal("selected_preset_index_changed", _selected_preset_index)
+
 var available_voice_languages: Array[String] = []
+
+var selected_preset: Preset:
+	get:
+		var index = select_preset_option_button.selected
+		if index == -1:
+			return null
+		else:
+			return presets[index]
 
 func _ready() -> void:
 	repopulate_select_preset()
@@ -60,14 +78,12 @@ func _on_delete_preset_button_pressed() -> void:
 	
 	# Select -1 to clear selection
 	select_preset_option_button.selected = -1
-	
+	select_preset_option_button.emit_signal("item_selected", select_preset_option_button.selected)
 
 
 func _on_select_preset_option_button_item_selected(index: int) -> void:
-	if index == -1:
-		preset_name_line_edit.text = ""
-	else:
-		preset_name_line_edit.text = presets[index].name
+	selected_preset_index = index
+	print("selected ", index)
 
 
 func _on_preset_name_line_edit_text_changed(new_text: String) -> void:
@@ -85,15 +101,24 @@ func _on_voice_language_line_edit_text_changed(new_text: String) -> void:
 
 
 func _on_tts_provider_option_button_item_selected(index: int) -> void:
-	voice_language_line_edit.clear()
-	var voice_languages
+	
 	if index == 0:
-		voice_languages = GTTS_VOICE_LANGUAGES.duplicate()
+		selected_preset.voice_provider = "gtts"
 	elif index == 1:
+		selected_preset.voice_provider = "edge"
+	load_voice_language_list()
+
+func load_voice_language_list():
+	var voice_languages
+	if selected_preset.voice_provider == "gtts":
+		voice_languages = GTTS_VOICE_LANGUAGES.duplicate()
+	elif selected_preset.voice_provider == "edge":
 		voice_languages = EDGE_TTS_VOICE_LANGUAGES.duplicate()
 	
+	voice_language_line_edit.clear()
 	available_voice_languages.assign(voice_languages)
 	filter_voices()
+	
 
 
 func filter_voices():
@@ -109,14 +134,41 @@ func filter_voices():
 	for voice in filtered_voices:
 		voice_language_item_list.add_item(voice)
 
-
+func update_selected_voice_language_label():
+	selected_voice_label.text = selected_preset.voice_language
 
 func _on_voice_language_item_list_item_selected(index: int) -> void:
 	if index == -1:
-		selected_voice_label.text = "None"
+		return
 	else:
-		selected_voice_label.text = voice_language_item_list.get_item_text(index)
+		selected_preset.voice_language = voice_language_item_list.get_item_text(index)
+		update_selected_voice_language_label()
 
 
 func _on_slow_check_button_pressed() -> void:
-	pass # Replace with function body.
+	selected_preset.slow = slow_check_button.button_pressed
+
+func _on_selected_preset_index_changed(new_index: int) -> void:
+	if new_index == -1:
+		preset_name_line_edit.text = ""
+	else:
+		load_preset_for_edit(presets[new_index])
+
+func load_preset_for_edit(preset: Preset):
+	preset_name_line_edit.text = preset.name
+	
+	if preset.voice_provider == "gtts":
+		tts_provider_option_button.select(0)
+	else: 
+		tts_provider_option_button.select(1)
+	load_voice_language_list()
+	
+	update_selected_voice_language_label()
+	
+	for i in voice_language_item_list.item_count:
+		if voice_language_item_list.get_item_text(i) == preset.voice_language:
+			voice_language_item_list.select(i)
+	
+	slow_check_button.button_pressed = preset.slow
+	
+	
